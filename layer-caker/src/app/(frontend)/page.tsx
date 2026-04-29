0cc9c98011b43aeeb4c47395337df0096da59fc0
Wilson Zhou <wilsonzhou56@gmail.com>
2026-04-29 15:21:50 -0400

    added components - post cards not working yet

--- layer-caker/src/app/(frontend)/page.tsx
+++ layer-caker/src/app/(frontend)/page.tsx
@@ -1,18 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { sanityFetch } from '@/sanity/lib/live'
+import { POSTS_QUERY } from '@/sanity/lib/queries'
 
 export default async function Page() {
+  const { data: posts } = await sanityFetch({ query: POSTS_QUERY })
+
   return (
-    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Scale Evolve and Innovate with FLEX-PAY</h1>
-      <h2 className="text-2xl font-italic">Redefining the meaning of Flexible Global Payment Infrastructure</h2>
-      <h3 className="text-3xl font-plain">WELCOME TO FLEXIBLE GLOBAL PAYMENT INFRASTRUCTURE</h3>  
-      <h2> Flex-Pay™ is transforming the global payment infrastructure—uniting nine dynamic payment types and a suite of additional services, all accessible through a single, seamless API.</h2> 
-
- 
-
-We simplify payment processes, advance financial inclusivity, and empower businesses to deliver exceptional user experiences in the ever-evolving world of digital commerce.    
+    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+      <h1 className="text-4xl font-bold">Post index</h1>
+      <ul className="grid grid-cols-1 divide-y divide-blue-100">
+        {posts.map((post) => (
+          <li key={post._id}>
+            <Link
+              className="block p-4 hover:text-blue-500"
+              href={`/posts/${post?.slug?.current}`}
+            >
+              {post?.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <hr />
-      <Link href="/posts">Flex-News &rarr;</Link>
-    </section>
-  );
+      <Link href="/">&larr; Return home</Link>
+    </main>
+  )
 }
