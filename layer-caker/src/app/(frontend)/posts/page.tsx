dd0562bf8d3ae922fe756a75f1471d3d470ec5b2
Wilson Zhou <wilsonzhou56@gmail.com>
2026-04-29 20:16:04 -0400

    tried to debug postcards but doesn't work yet

--- layer-caker/src/app/(frontend)/posts/page.tsx
+++ layer-caker/src/app/(frontend)/posts/page.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import { sanityFetch } from "@/sanity/lib/live";
-import { POSTS_QUERY } from "@/sanity/lib/queries";
+import Link from 'next/link'
+import { sanityFetch } from '@/sanity/lib/live'
+import { POSTS_QUERY } from '@/sanity/lib/queries'
 
 export default async function Page() {
-  const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
+  const { data: posts } = await sanityFetch({ query: POSTS_QUERY })
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Flex-News</h1>
+      <h1 className="text-4xl font-bold">Post index</h1>
       <ul className="grid grid-cols-1 divide-y divide-blue-100">
         {posts.map((post) => (
           <li key={post._id}>
@@ -23,5 +23,5 @@ export default async function Page() {
       <hr />
       <Link href="/">&larr; Return home</Link>
     </main>
-  );
+  )
 }
