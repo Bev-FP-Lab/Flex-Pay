c03a46763ebf87f137adde1b6c70975b9dee29c3
Wilson Zhou <wilsonzhou56@gmail.com>
2026-05-03 23:23:03 -0400

    added significant updates to contact page and fixed sign up button completely

--- layer-caker/src/app/(frontend)/bottomheader/careers/page.tsx
+++ layer-caker/src/app/(frontend)/bottomheader/careers/page.tsx
@@ -7,12 +7,81 @@ import { TitleThree } from '@/components/titlethree'
 
 export default async function Page() {
   return (
-    <section className="container mx-auto grid grid-cols-1 gap-6 p-12 bg-indigo-800 ">
+    <><section className="container mx-auto grid grid-cols-1 gap-6 p-12 bg-indigo-800 ">
       <TitleThree>JOIN FLEXPAY</TitleThree>
 
 
     </section>
+    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
 
+     <h2 className="text-[20px] font-sans text-center mx-30">Bring your passion, ideas and strong work ethic to FLEXPAY. We love what we do, who we work with and we thrive on each other's energy.
+      <br></br> We work hard and we have a good time.</h2>
+    <h1 className="text-[30px] font-sans"> UI DESIGNER - PAID INTERNSHIP </h1>
+
+    <h2 className="text-[15px] font-sans"> <p id="u3482-4">We are looking for one (1) <b><u>UI Designer</u></b> to join our team.</p>
+    <p id="u3482-6">(Paid internship with chances of joining the team full time)</p><p id="u3482-7">&nbsp;</p><p id="u3482-9"><b><u>Job Description</u></b></p><p id="u3482-11">We are looking for UI designer student interns with experience and talent to design and shape unique, user-centric products and experiences. The ideal candidate will have experience working in agile teams, with developers, UX designers and copywriters. You will be able to make deliberate design decisions and to translate any given user-experience journey into a smooth and intuitive interaction.</p>
+    <p id="u3482-12">&nbsp;</p><p id="u3482-14"><b><u>Responsibilities</u></b></p><p id="u3482-16">- Be a great team player, experienced in working with agile teams. Ability to collaborate closely with developers, copywriters and UX designers.</p><p id="u3482-18">- Develop standard UI components and style guides for company-wide use.</p><p id="u3482-20">- Create, improve, and use wireframes, prototypes, style guides, user flows, and effectively communicate your interaction ideas to developers and other key stakeholders.</p><p id="u3482-22">- Address product, marketing, and business needs.</p><p id="u3482-23">&nbsp;</p><p id="u3482-25"><span id="u3482-24">Skills &amp; experience</span></p><p id="u3482-27">- Strong portfolio to be presented and defended during interview.</p><p id="u3482-29">- Experience with web and mobile app design.</p><p id="u3482-31">- Working knowledge of responsive design and grid principles.</p><p id="u3482-33">- Hands-on experience creating sitemaps, wireframes, lo-fi and hi-fi layouts, prototypes, storyboards, user flows, etc.</p><p id="u3482-35">- Strong typography, layout, and visual design skills.</p><p id="u3482-37">- Working knowledge of industry tools such as: Photoshop, Sketch, Illustrator, InVision, InDesign, Balsamiq, Omnigraffle, UXPin, Quartz.</p><p id="u3482-39">- Understanding of basic front-end languages: HTML5, CSS3 Javascript.</p><p id="u3482-41">- Creative ideas with problem solving mindset.</p><p id="u3482-43">- Experience working in an agile/scrum development process.</p><p id="u3482-45">- Be opened to receiving objective criticism and improving upon it.</p><p id="u3482-46">&nbsp;</p><p id="u3482-48"><span id="u3482-47">Is this job for you?</span></p><p id="u3482-49">&nbsp;</p><p id="u3482-51">Are you already becoming technically skilled in UI?</p><p id="u3482-52">&nbsp;</p><p id="u3482-54">You must demonstrate strong initiative for building up your own UI skills. Be prepared to describe what you are already learning and designs you have created.</p><p id="u3482-55">&nbsp;</p><p id="u3482-57">We don’t expect you to be an expert UI designer, but you must be able take existing models and best practices and adapt them to our environment.</p><p id="u3482-58">&nbsp;</p><p id="u3482-60">Be ready to show us designs you have created and discuss them in detail.</p><p id="u3482-61">&nbsp;</p><p id="u3482-63">Have you designed apps?</p><p id="u3482-64">&nbsp;</p><p id="u3482-66">You will be required to deploy new designs into production by building creative and responsive layouts and prototypes using modern UI design best practices. Be ready to talk about your experiences doing mock-ups, designing interfaces, and sitemaps. You should have strong opinions about best web and app design practices.</p><p id="u3482-67">&nbsp;</p><p id="u3482-69">We have a number of prospective projects. A major responsibility of this job is helping our leadership evaluate them and identifying the top candidates. You should expect priorities and direction to change as we begin developing new projects.</p><p id="u3482-70">&nbsp;</p><p id="u3482-72">Do you have experience working in dynamic environments (such as a Startup)? Are you excited by ambiguous goals and changing requirements?</p><p id="u3482-73">&nbsp;</p><p id="u3482-75"><span id="u3482-74">Location</span></p><p id="u3482-77">Toronto, ON (Remote)</p><p id="u3482-78">&nbsp;</p><p id="u3482-80"><span id="u3482-79">Compensation</span></p><p id="u3482-82">$20.00 per hour</p><p id="u3482-83">&nbsp;</p><p id="u3482-85"><span id="u3482-84">Start Date</span></p><p id="u3482-87">September 2025</p><p id="u3482-88">&nbsp;</p><p id="u3482-90">Please note: This is a paid internship with an opportunity to become a full-time</p><p id="u3482-92">Please send your resume and cover letter to careers@flex-pay.io . Also, if you have a portfolio showcasing your work please send it as well.</p><p id="u3482-94">Thank you for your interest, qualified candidates will be contacted.</p></h2>
+
+    </section>
+    <section className="container mx-auto grid grid-cols-1 gap-6 p-12 bg-linear-to-r from-black to-red-900 content-center">
+
+        <h1 className="text-1xl md:text-2xl lg:text-3xl text-slate-800 text-pretty text-white font-sans my-7 text-center text-plain"> Get in touch with us today to discover how Flex-Pay is shaping the future of global payments. </h1>
+        <Link
+          href="/signup"
+        >
+
+          <button className="bg-white hover:bg-purple-900 text-black font-bold py-3 px-135 rounded-full">
+            SIGN UP
+          </button>
+        </Link>
+
+      </section><section className="container mx-auto grid grid-cols-2 gap-6 p-12 bg-purple-900 content-center">
+
+
+        <Link href="/bottomheader/aboutus">
+
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> About Us </h1>
+
+
+
+        </Link>
+        <Link
+          href="/bottomheader/legal"
+        >
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> Legal </h1>
+
+
+        </Link>
+        <Link
+          href="/bottomheader/careers"
+        >
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> Careers </h1>
+
+
+        </Link>
+        <Link
+          href="/bottomheader/legal"
+        >
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> Privacy </h1>
+
+
+        </Link>
+        <Link
+          href="/bottomheader/contactus"
+        >
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> Contact Us </h1>
+
+
+        </Link>
+        <Link
+          href="/bottomheader/legal"
+        >
+          <h1 className="text-0.5xl md:text-0.75xl lg:text-1xl text-slate-800 text-pretty text-white font-sans my-2 text-center text-plain"> Terms & Conditions </h1>
+
+
+        </Link>
+
+      </section></>
   )
   
 }
